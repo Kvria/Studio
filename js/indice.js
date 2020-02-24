ef9e4121a93f624a0d6ca4d429ef3c96ac7d2c28
Kvria <trishakuria@gmail.com>
2020-02-24 18:07:12 +0300

    add final api chimp and other final changes to file

--- js/indice.js
+++ js/indice.js
@@ -11,10 +11,31 @@ $(document).ready(function(){
     $(".prodIcon").toggle();
     $(".product").toggle();
   });
+
+
   $(".image1").hover(function(){
+  
     $(".text1").toggle();
   });
   $(".image2").hover(function(){
     $(".text2").toggle();
+  });
+  $(".image3").hover(function(){
+    $(".text3").toggle();
+  });
+  $(".image4").hover(function(){
+    $(".text4").toggle();
+  });
+  $(".image5").hover(function(){
+    $(".text5").toggle();
+  });
+  $(".image6").hover(function(){
+    $(".text6").toggle();
+  });
+  $(".image7").hover(function(){
+    $(".text7").toggle();
+  });
+  $(".image8").hover(function(){
+    $(".text8").toggle();
   });
 });
